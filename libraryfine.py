4a56836e478ba6f34ee7d733b13391d72704fea2
janmejaysinghh <95363621+janmejaysinghh@users.noreply.github.com>
2022-05-20 13:34:51 +0530

    Add files via upload

---
+++ libraryfine.py
@@ -0,0 +1,13 @@
+d=int(input("Enter the day"))
+if d<5:
+ a=d*2
+ print("Your fine is:",a)
+if d>=6 and d<=10:
+ a=d*3
+ print("Your fine is:",a)
+if d>=11 and d<=15:
+ a=d*4
+ print("Your fine is:",a)
+if d>15:
+ a=d*5
+ print("Your fine is:",a)
